1ea54917b7b05e48fdda6b65c553a14b21aadca1
quickthom <thom@jonesrowell.com>
2021-12-12 01:13:38 -0500

    Day 12

--- 2021/12.py
+++ 2021/12.py
@@ -3,50 +3,33 @@ from collections import defaultdict
 
 lines = read(12)
 
-caves = dict()
-
-class Cave:
-    def __init__(self, name):
-        self.name = name
-        self.links = set()
-        self.size = 'small' if self.name.islower() else 'large'
-    def link_to(self, cave):
-        self.links.add(cave)
-
-for line in lines:
-    c1, c2 = line.split('-')
-    if c1 not in caves:
-        caves[c1] = Cave(c1)
-    if c2 not in caves:
-        caves[c2] = Cave(c2)
-    caves[c1].link_to(caves[c2])
-    caves[c2].link_to(caves[c1])
-
-def can_go(cave, visited):
-    if cave.name =='start':
+def can_go(cave, small_visits):
+    if cave == 'start':
         return False
-    elif cave.size == 'large':
+    elif cave.isupper():
         return True
-    elif len(visited) == 0 or max(visited.values()) > 1:
-        return visited[cave] < 1
+    elif len(small_visits) == 0 or max(small_visits.values()) > 1:
+        return small_visits[cave] < 1
     else:
-        return visited[cave] < 2
-    return False
+        return small_visits[cave] < 2
 
-def reach_end_from(cave, visited):
+def reach_end_from(cave, small_visits):
     routes = 0
-    for dest in cave.links:
-        if dest.name == 'end':
+    for dest in links[cave]:
+        if dest == 'end':
             routes += 1
-        elif not can_go(dest, visited):
-            continue
-        else:
-            v = visited.copy()
-            if dest.size == 'small':
-                v[dest] += 1
-            routes += reach_end_from(dest,v)
+        elif can_go(dest, small_visits):
+            sv = small_visits.copy()
+            if dest.islower():
+                sv[dest] += 1
+            routes += reach_end_from(dest,sv)
     return routes
 
+small_visits = defaultdict(int)
+links = defaultdict(set)
 
-visited = defaultdict(int)
-print(reach_end_from(caves['start'], visited))
+for c1, c2 in [x.split('-') for x in lines]:
+    links[c1].add(c2)
+    links[c2].add(c1)
+
+print(reach_end_from('start', small_visits))
